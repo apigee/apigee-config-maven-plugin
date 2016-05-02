49a15431cc917e72a63f8fd92efd48508249348b
Madhan Sadasivam <msadasivam@apigee.com>
2016-05-02 16:57:44 +1000

    license updated.

--- src/main/java/com/apigee/edge/config/mavenplugin/CacheMojo.java
+++ src/main/java/com/apigee/edge/config/mavenplugin/CacheMojo.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2014 Apigee Corporation
+ * Copyright (C) 2016 Apigee Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
